400301cbf8f08fe0d794e3c3a3d86e87168441db
f1gar0s <125069408+f1gar0s@users.noreply.github.com>
2023-03-24 18:36:41 +0300

    Add files via upload

---
+++ C#/1общий/Class1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyClass
+{
+    internal class Book
+    {
+        private string author; // автор
+        private string title; // название
+        private string publisher; // издательство
+        private int pages; // кол-во страниц
+        private int year; // год издания
+   
+        private static double price = 9;
+
+        public void SetBook(String author, string title, string publisher, int pages, int year)
+        {
+            this.author = author;
+            this.title = title;
+            this.publisher = publisher;
+            this.pages = pages;
+            this.year = year;
+
+        }
+        public static void SetPrice(double price)
+        {
+            Book.price = price;
+        }
+        public void Show()
+        {
+            Console.WriteLine("\nКнига:\nАвтор: {0}\nНазвание: {1}\nГод издания: {2}\nИздательство: {3}\n{4}стр.\nСтоимость аренды: {5}", author, title, year, publisher, pages, Book.price);
+        }
+        public double PriceBook(int s)
+        {
+            double cust = s * price;
+            return cust;
+        }
+    }
+    
+    
+}
---
+++ C#/1общий/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyClass
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Book b1 = new Book();
+            b1.SetBook("Пушкин А.С.", "Капитанская дочка", "Вильямс", 123, 2012);
+            Book.SetPrice(12);
+            b1.Show();
+            Console.WriteLine("\n Итоговая стоимость аренды: {0} p.",b1.PriceBook(3));
+
+        }
+    }
+}
